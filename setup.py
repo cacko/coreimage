7fdce9e7fbf83d97927ac57b66385cc6bc42bb58
cacko <alex@cacko.net>
2023-08-02 00:19:22 +0100

    changes

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         "emoji>=2.4.0",
         "pydantic>=1.10.8",
         "cv2-collage-v2>=0.11",
-        "corelog"
+        "corelog>0.0.11"
     ],
     setup_requires=["wheel", "semver"],
     python_requires=">=3.11",
