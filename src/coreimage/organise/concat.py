9f3912e80d77964137af06df2ffb116944d8bf0c
cacko <alex@cacko.net>
2023-08-30 12:04:32 +0100

    Make that it works in 90% of the cases.  3:30.

--- src/coreimage/organise/concat.py
+++ src/coreimage/organise/concat.py
@@ -100,7 +100,7 @@ class Concat:
             background=(0, 0, 0),
             max_width=500
     ):
-        aspectratiofactor = max(0.315, 4 / len(imgList)),
+        aspectratiofactor = max(0.315, 4 / len(imgList))
 
         [img.thumbnail((randint(300, max_width), img.height), Image.Resampling.LANCZOS)
          if img.width > max_width else img for img in imgList
