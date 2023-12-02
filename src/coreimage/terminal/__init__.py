1f5d40f4570cec7b590a8d426a8c93c41919a816
cacko <alex@cacko.net>
2023-12-02 02:48:35 +0000

    "update taskfile.yaml for 1ca6daf..f18dad8 [ci skip] (gh-16636) 📍“diff --git✓❀❌❝"

--- src/coreimage/terminal/__init__.py
+++ src/coreimage/terminal/__init__.py
@@ -1,6 +1,29 @@
 from coreimage.terminal.kitty import get_term_image as get_kitty_image
+from term_image.image import KittyImage
+from pathlib import Path
+from typing import Any, Optional
+import numpy as np
+from PIL import Image
 
+def print_term_image(
+    image: Optional[np.ndarray | Image.Image] = None,
+    image_path: Optional[Path] = None,
+    height: Optional[int]=10,
+    **kwds
+):
+    try:
+        assert KittyImage.is_supported()
+        kwds.setdefault("height", height)
+        with get_kitty_image(
+            image=image,
+            image_path=image_path,
+            **kwds
+        ) as term_image:
+            print(term_image)
+    except AssertionError:
+        pass
 
 __all__ = [
-    "get_kitty_image"
+    "get_kitty_image",
+    "print_term_image"
 ]
--- src/coreimage/terminal/kitty.py
+++ src/coreimage/terminal/kitty.py
@@ -4,29 +4,34 @@ from typing import Any, Optional
 import cv2
 import numpy as np
 from PIL import Image
+from contextlib import contextmanager
+
+@contextmanager
+def get_image(
+    image_data: Optional[np.ndarray | Image.Image | Path] = None,
+    **kwds
+) -> Optional[Image.Image]:
+    try:
+        match(image_data):
+            case Image.Image():
+                yield KittyImage(image=image_data.convert("RGB"),  **kwds)
+            case np.ndarray():
+                yield KittyImage(image=Image.fromarray(cv2.cvtColor(image_data, cv2.COLOR_BGR2RGB)), **kwds)
+            case Path():
+                yield KittyImage(image=Image.open(image_data.as_posix()).convert("RGB"),  **kwds)
+    finally:
+        pass
 
 
+@contextmanager
 def get_term_image(
     image: Optional[np.ndarray | Image.Image] = None,
     image_path: Optional[Path] = None,
-    height=10,
-    fmt="{:1.1#}",
     **kwds
 ):
-
-    def get_image(image_data: Any) -> Optional[Image.Image]:
-        match(image_data):
-            case Image.Image():
-                return image_data
-            case np.ndarray():
-                return Image.fromarray(cv2.cvtColor(image_data, cv2.COLOR_BGR2RGB))
-        return None
-
-    kitty_image = None
-    if image is not None:
-        kitty_image = KittyImage(get_image(image), height=height, **kwds)
-    if image_path and all([image_path.exists(), image_path.is_file()]):
-        cvimage = cv2.imread(image_path.as_posix())
-        kitty_image = KittyImage(get_image(cvimage), height=height, **kwds)
-    if kitty_image:
-        return fmt.format(kitty_image)
+    try:
+        with get_image(next(filter(None, [image, image_path])), **kwds) as kitty_image:
+            fmt = kwds.get("fmt", "{:1.1#}")
+            yield fmt.format(kitty_image)
+    finally:
+        pass
