458b2ba4748d162d37772ade444e7528e1d6c826
cacko <alex@cacko.net>
2024-01-25 09:27:36 +0000

    "add setup.py diff to diff diff diff output. closes  [yuyeow] 😁–…—•‍“”‘’]"

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         "segno>=1.5.2",
         "spandrel>0.2",
         "torch>=2.1.2",
-        "torchvision=>0.16.2"
+        "torchvision>=0.16.2"
     ],
     setup_requires=["wheel", "semver"],
     python_requires=">=3.11",
