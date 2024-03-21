b2ac5bf84050b1e18f1312dcd14384e2e09443a9
cacko <alex@cacko.net>
2024-03-21 03:55:59 +0000

    "fix coreimage.find.py to work with python 2.7.x and 3.9.x. This is a hacky fix.  thanks @uhnichertiel. “hopefully!!”"

--- src/coreimage/find.py
+++ src/coreimage/find.py
@@ -3,10 +3,10 @@ from corefile import filepath
 from .utils import IMAGE_EXT
 
 
-def find_images(src: list[Path]):
+def find_images(src: list[Path|str]):
     suffixes = IMAGE_EXT.get_suffixes()
     for pth in src:
-        pth = Path(pth)
+        pth = Path(pth) if isinstance(pth , str) else pth
         yield from filepath(
             root=pth,
             suffixes=suffixes
