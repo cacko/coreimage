978c44ca1a0c6e74f29cc21479730028aba023d7
cacko <alex@cacko.net>
2024-05-31 12:39:34 +0100

    "update kitty.py to match the new version of the repo. 😁–“”—‘’•…‍‎ ‏​ ․†›‚‛�"

--- src/coreimage/terminal/kitty.py
+++ src/coreimage/terminal/kitty.py
@@ -2,24 +2,37 @@ from pathlib import Path
 from typing import Generator, Optional
 import cv2
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageOps
 from contextlib import contextmanager
+
 
 @contextmanager
 def get_image(
-    image_data: Optional[np.ndarray | Image.Image | Path] = None,
-    **kwds
+    image_data: Optional[np.ndarray | Image.Image | Path] = None, **kwds
 ) -> Generator[Image.Image, None, None]:
     try:
         from term_image.image import KittyImage
+
         assert KittyImage.is_supported()
-        match(image_data):
+        match (image_data):
             case Image.Image():
-                yield KittyImage(image=image_data.convert("RGB"),  **kwds)
+                yield KittyImage(
+                    image=ImageOps.exif_transpose(image_data.convert("RGB")), **kwds
+                )
             case np.ndarray():
-                yield KittyImage(image=Image.fromarray(cv2.cvtColor(image_data, cv2.COLOR_BGR2RGB)), **kwds)
+                yield KittyImage(
+                    image=ImageOps.exif_transpose(
+                        Image.fromarray(cv2.cvtColor(image_data, cv2.COLOR_BGR2RGB))
+                    ),
+                    **kwds,
+                )
             case Path():
-                yield KittyImage(image=Image.open(image_data.as_posix()).convert("RGB"),  **kwds)
+                yield KittyImage(
+                    image=ImageOps.exif_transpose(
+                        Image.open(image_data.as_posix()).convert("RGB")
+                    ),
+                    **kwds,
+                )
     except (AssertionError, ValueError):
         raise NotImplementedError("kirtty is not support on this termianl")
     finally:
@@ -30,10 +43,12 @@ def get_image(
 def get_term_image(
     image: Optional[np.ndarray | Image.Image] = None,
     image_path: Optional[Path] = None,
-    **kwds
+    **kwds,
 ):
     try:
-        with get_image(next(filter(lambda x: x is not None, [image, image_path])), **kwds) as kitty_image:
+        with get_image(
+            next(filter(lambda x: x is not None, [image, image_path])), **kwds
+        ) as kitty_image:
             fmt = kwds.get("fmt", "{:1.1#}")
             yield fmt.format(kitty_image)
     finally:
