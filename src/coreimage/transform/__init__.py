5a0fe301716284630b5f810b7277228dd80d9feb
cacko <alex@cacko.net>
2024-05-31 12:27:36 +0100

    "improve diff for coreimage coreimage-1.3.0-beta.1.0.0_ppt_8_c1_1_e_4_l3e_3_0__1"

--- src/coreimage/transform/__init__.py
+++ src/coreimage/transform/__init__.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from typing import Optional
-from PIL import Image
+from PIL import Image, ImageOps
 from coreimage.utils import round8
 from .crop import Cropper
 from .upscale import Upscale
@@ -13,11 +13,14 @@ __all__ = [
 
 
 def normalize(
-    paths: list[Path],
+    paths: list[Path]|Path,
     max_size: Optional[int] = None,
 ):
+    if not isinstance(paths, list):
+        paths = [paths]
     for pth in paths:
         im = Image.open(pth.as_posix())
+        ImageOps.exif_transpose(im, in_place=True)
         if not max_size:
             max_size = max(im.width, im.height)
         max_size = round8(max_size)
